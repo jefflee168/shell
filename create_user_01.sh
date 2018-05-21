3dd1a13c4ddc4aa38e66f51d07bac76e5b7d3861
jeff <admin@example.com>
2018-05-21 15:59:44 +0800

    change create_user_01.sh

--- create_user_01.sh
+++ create_user_01.sh
@@ -1,14 +1,17 @@
-#/bin/bash
+#!/bin/bash
 
-CMD=`cat /etc/passwd|grep 'test' -c`
-echo "Please enter your name: "
+# 此脚本为创建用户脚本，当用户输入用户名是，脚本会先判断是否存在
+# 如果不存在，则会创建，并且会新增加的用户添加到文件/etc/sudoers中
+
+echo "Please enter new user name:"
 read USER
-if [ $CMD -eq 0 ];then
-    useradd $USER
-    chmod +w /etc/sudoers
-    echo "$USER ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
-    chmod -w /etc/sudoers
-    echo "Congratulations,$USER added."
+CMD=`grep $USER /etc/passwd | sed 's/:/ /g' | awk '{print $1}'`
+if [ $CMD == $USER ];then
+    echo "Oops!$USER exists.Please enter another."
 else
-    echo "$USER exists."
+    adduser $USER
+    chmod +w /etc/sudoers
+    sed -i "/root[[:space:]]ALL=(ALL:ALL) ALL/a\\$USER\tALL=(ALL:ALL) NOPASSWD: ALL" /etc/sudoers
+    chmod -w /etc/sudoers
+    echo "INFO: Congratulations,$USER added."
 fi
